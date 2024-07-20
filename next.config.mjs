60303432d019038b008fc2ed90b1f268ecd2550e
thetnaungsoe-mmocc <thetnaungsoe.mmocc@gmail.com>
2024-07-20 21:43:46 +0630

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // Use the `GITHUB_REPOSITORY` environment variable to determine the repository name
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+const nextConfig = {
+  images: {
+    unoptimized: true, // Disable image optimization for static site hosting
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
 
 export default nextConfig;
